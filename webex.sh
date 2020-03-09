245de1b8f0d9d5f49ce8c50781a211dd8d628075
Rob van der Kind <robvand@cisco.com>
2020-03-09 14:14:58 +0100

    ibla

--- webex.sh
+++ webex.sh
@@ -1,5 +1,5 @@
 curl -H "Content-Type: application/json" \
-    -H 'Authorization: Bearer ODc5ZDVmZGUtM2Q0Yy00MzM3LTk2YmItYjlhYzZkNTZjN2VmYTk3ZjdlY2ItOTEx_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f' \
+    -H 'Authorization: Bearer $bearer' \
     -i -v -X POST \
-    -d "{ \"roomId\": \"Y2lzY29zcGFyazovL3VzL1JPT00vYmMzNGIyZjAtNjFmYS0xMWVhLWI4NTgtMTM5NTgxNTJhYmNl\", \"text\": \"hi\" }" \
+    -d "{ \"roomId\": \"$roomId\", \"text\": \"hi\" }" \
     https://api.ciscospark.com/v1/messages
