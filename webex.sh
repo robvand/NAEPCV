0d6e07a38dc739f54445e5c61397ce891a1b8a2d
Rob van der Kind <robvand@cisco.com>
2020-03-09 19:49:30 +0100

    push

--- webex.sh
+++ webex.sh
@@ -1,6 +1,11 @@
+#post output of PCV query to Webex Teams room
 curl --request POST \
 --header "Authorization: Bearer ODc5ZDVmZGUtM2Q0Yy00MzM3LTk2YmItYjlhYzZkNTZjN2VmYTk3ZjdlY2ItOTEx_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f" \
 --form "files=@output_"$CI_PIPELINE_ID.txt"" \
 --form "roomId=Y2lzY29zcGFyazovL3VzL1JPT00vYmMzNGIyZjAtNjFmYS0xMWVhLWI4NTgtMTM5NTgxNTJhYmNl" \
 --form "text=pipeline results" \
 https://api.ciscospark.com/v1/messages
+
+#todo get specific output only
+#cat output_"$CI_PIPELINE_ID".txt | grep "Later Epoch Smart Events" -A 13 > tmp.txt
+#cat output_"$CI_PIPELINE_ID".txt | grep msg >> tmp.txt
